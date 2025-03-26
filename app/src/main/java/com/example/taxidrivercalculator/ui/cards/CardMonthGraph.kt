78b00739afe382957e1ee22353b7c77089c92ccb
Alex <actorlefter@gmail.com>
2025-03-26 21:53:37 +0300

    Refactor: Enhance chart visualization with new styling and features
    -   Update grid properties to include axis properties and remove default grid lines.
    -   Adjust label properties for better visibility, including text color.
    -   Add indicator properties with custom text styles for horizontal indicators.
    -   Add animationMode with delay to make the animation more visible
    -   Update Line properties:
        - Update the goal reach line: Add gradient fill, add animation and delay, and add a custom stroke style.
       -  Update max value for better visualization

--- app/src/main/java/com/example/taxidrivercalculator/ui/cards/CardMonthGraph.kt
+++ app/src/main/java/com/example/taxidrivercalculator/ui/cards/CardMonthGraph.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,10 +24,12 @@ import com.google.android.material.snackbar.BaseTransientBottomBar.AnimationMode
 import ir.ehsannarmani.compose_charts.LineChart
 import ir.ehsannarmani.compose_charts.models.GridProperties
 import ir.ehsannarmani.compose_charts.models.GridProperties.AxisProperties
+import ir.ehsannarmani.compose_charts.models.HorizontalIndicatorProperties
 import ir.ehsannarmani.compose_charts.models.LabelProperties
 import ir.ehsannarmani.compose_charts.models.Line
 import ir.ehsannarmani.compose_charts.models.LineProperties
 import ir.ehsannarmani.compose_charts.models.StrokeStyle
+import ir.ehsannarmani.compose_charts.models.VerticalIndicatorProperties
 import ir.ehsannarmani.compose_charts.models.ZeroLineProperties
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -35,24 +39,29 @@ class CardMonthGraph {
     @Composable
     fun DrawMonthGraphCard(chartData: StateFlow<List<Double>>) {
         val chartState by chartData.collectAsStateWithLifecycle()
-        val max = chartState.maxOrNull() ?: 0.0
+        val max = chartState.maxOrNull() ?: 0.0 //TODO: goal
         val min = chartState.minOrNull() ?: 0.0
 
         val axisProperties = AxisProperties(
-            enabled = true
+            enabled = true,
+            color = SolidColor(Color(0xFFFF0000)),
             /*style = StrokeStyle.Dashed(intervals = floatArrayOf(10f,10f)),
             color = SolidColor(Color(0xFF0000FF)),
             thickness = (.5).dp,
             lineCount = 5*/
         )
         val labelProperties = LabelProperties(
-            enabled = false,
-            textStyle = MaterialTheme.typography.labelSmall,
-            labels = listOf("1", "2")
+            enabled = true,
+            textStyle = TextStyle(color = Color.Blue)
         )
         val gridProperties = GridProperties(
             enabled = false,
-            yAxisProperties = axisProperties
+            yAxisProperties = axisProperties,
+            xAxisProperties = axisProperties
+        )
+        val indicatorProperties = HorizontalIndicatorProperties(
+            enabled = true,
+            textStyle = TextStyle(color = Color.White),
         )
 
         BaseCard {
@@ -63,19 +72,30 @@ class CardMonthGraph {
                             label = "Goal reach",
                             values = chartState,
                             color = SolidColor(Color(0xFF23af92)),
+                            firstGradientFillColor = Color(0xFF2BC0A1).copy(alpha = .5f),
+                            secondGradientFillColor = Color.Transparent,
+                            strokeAnimationSpec = tween(2000, easing = EaseInOutCubic),
+                            gradientAnimationDelay = 1000,
+                            drawStyle = ir.ehsannarmani.compose_charts.models.DrawStyle.Stroke(width = 2.dp)
                         )
                         /*Line(
-                            label = "Zero",
+                            label = "Goal",
                             values = listOf(0.0, 0.0),
                             color = SolidColor(Color(0xFFFF0000)),
                         )*/
                     )
                 },
+                //lineProperties = labelProperties,
+                animationMode = ir.ehsannarmani.compose_charts.models.
+                AnimationMode.Together(delayBuilder = {
+                    it * 500L
+                }),
                 gridProperties = gridProperties,
                 zeroLineProperties = ZeroLineProperties(
-                    enabled = true,
+                    enabled = false,
                     color = SolidColor(Color(0xFFFFFFFF)),
                 ),
+                indicatorProperties = indicatorProperties,
                 labelProperties = labelProperties,
                 minValue = min,
                 maxValue = max*1.2,
@@ -90,7 +110,7 @@ class CardMonthGraph {
 
         val previewData = remember {
             MutableStateFlow(
-                listOf( 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+                listOf( 0.0, 2.0, 3.0, 7.0, 10.0, 12.0, 18.0, 25.0, 27.0, 30.0
                 )
             )
         }
