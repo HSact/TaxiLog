9fc2b32f467a5f269f9bb84ef380d8394ed5a8dd
Alex <actorlefter@gmail.com>
2025-09-03 03:29:31 +0300

    Refactor: Clean up unused imports in `CardDaysInMonthGraph.kt`
    - Removed unused imports:
        - `androidx.compose.foundation.layout.fillMaxWidth`
        - `androidx.compose.foundation.layout.wrapContentWidth`
        - `androidx.compose.material3.MaterialTheme`
        - `androidx.compose.material3.Text`
        - `androidx.compose.ui.Alignment`

--- app/src/main/java/com/hsact/taxilog/ui/cards/CardDaysInMonthGraph.kt
+++ app/src/main/java/com/hsact/taxilog/ui/cards/CardDaysInMonthGraph.kt
@@ -3,17 +3,12 @@ package com.hsact.taxilog.ui.cards
 import androidx.compose.animation.core.EaseInOutCubic
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
