ef08534af29f1c35c766bb1c37d708d77ba8b025
Alex <actorlefter@gmail.com>
2026-08-21 07:34:56 +0300

    UI: Disable interaction on last shift card when no data is available
    - Updated `CardLastShift.kt` to set the `clickable` modifier's `enabled` state to false if `lastShiftValue` is null, preventing users from clicking an empty card.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
     alias(libs.plugins.crashlytics)
 }
 
-val vName = "1.5.0"
-val vCode = 15
+val vName = "1.6.0"
+val vCode = 16
 android {
     namespace = "com.hsact.taxilog"
     compileSdk =
